fa97ca28c8886dea8e5a8dfde020494629151f89
test_user <osoba.udon@gmail.com>
2017-09-26 08:09:59 +0900

    add test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+class hoge(object):
+
+    target = {}
+
+    def __init__(self):
+
+        #self.target = {}
+
+        pass
+
+
+
+    def append(self, val, key):
+
+        if self.target.has_key(key):
+
+            self.target[key] = self.target[key] + val
+
+        else:
+
+            self.target[key] = val
+
+
+
+if __name__ == '__main__':
+
+    key = "test"
+    a = hoge()
+
+    a.append(1, key)
+    print("{0}".format(a.target[key]))
+
+    b = hoge()
+
+    b.append(1, key)
+
+    print("{0}".format(b.target[key]))
